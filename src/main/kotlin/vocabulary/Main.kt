5a077c45ec198e9f81d3b3eb4f1c3975a95c527b
Andrei Larionov <0658203@gmail.com>
2023-06-22 18:03:13 -0300

    LEWTB-2 adds nullable correctAnswersCount

--- src/main/kotlin/vocabulary/Main.kt
+++ src/main/kotlin/vocabulary/Main.kt
@@ -9,21 +9,9 @@ fun main() {
     val lines = wordsFile.readLines()
     for (line in lines) {
         val line = line.split("|")
-        val word = Word(line[0], line[1])
+        val word = Word(line[0], line[1],null ?: 0 )
         dictionary.add(word)
     }
-// оставил код, чтобы показать как обновил файл
-//    val newWords = mutableListOf<String>()
-//    for (word in dictionary) {
-//        newWords.add("${word.original}|${word.translate}|${word.correctAnswersCount}")
-//    }
-//
-//    wordsFile.writeText("")
-//
-//    for (word in newWords) {
-//        wordsFile.appendText(word)
-//        wordsFile.appendText("\n")
-//    }
 
     for (word in dictionary) {
         println(word)
